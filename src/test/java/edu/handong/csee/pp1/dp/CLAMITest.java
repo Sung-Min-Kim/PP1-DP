e40ef46e5a54b746cf9d401982cb240949fb5d29
sungminkim <ksm3169@gmail.com>
2018-10-23 22:32:05 +0900

    Finished Task 4 & Task 5

--- src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
+++ src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
@@ -85,7 +85,7 @@ public class CLAMITest {
 		
 		// TASK5 TODO Use this method to find the best cutoff
 		
-		String bestCutoff = "50";
+		String bestCutoff = "88";
 		
 		String[] args = {"-f","data/Apache.arff","-l","isDefective","-p", "TRUE", "-m", "-c", bestCutoff};
 
@@ -97,7 +97,7 @@ public class CLAMITest {
 		
 		// TASK6 TODO Use this method to find the best cutoff for f-measure
 		
-		String bestCutoff = "50";
+		String bestCutoff = "42";
 		
 		String[] args = {"-f","data/Apache.arff","-l","isDefective","-p", "TRUE", "-m", "-c", bestCutoff};
 
